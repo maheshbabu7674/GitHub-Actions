39a265a9c13b64395746e1c05ce6fd7e4d089dcc
nayaki laxminarayana <nayakilaxminarayana99@outlook.com>
2024-08-22 08:26:52 +0000

    Added variables.tf

---
+++ resoucegroups/striker-rg/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "striker-rg"
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  default     = "eastus2"
+}
