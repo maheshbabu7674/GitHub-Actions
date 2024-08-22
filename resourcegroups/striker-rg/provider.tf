3692f7a5096d06ab7a975b035df2e7f52db62051
nayaki laxminarayana <nayakilaxminarayana99@outlook.com>
2024-08-22 09:41:55 +0000

    Updated provider.tf

--- resourcegroups/striker-rg/provider.tf
+++ resourcegroups/striker-rg/provider.tf
@@ -1,13 +1,8 @@
 terraform {
-  required_version = ">= 1.3.0"
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = ">= 1.3.0"
-    }
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      source = "hashicorp/azurerm"
+      version = "3.116.0"
     }
   }
 }
