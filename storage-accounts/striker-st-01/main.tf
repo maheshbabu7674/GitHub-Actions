c427d4c2c63f14aa73667331855650db18fe93e0
nayaki laxminarayana <nayakilaxminarayana99@outlook.com>
2024-08-22 12:09:16 +0000

    Updated main.tf

--- storage-accounts/striker-st-01/main.tf
+++
@@ -1,7 +0,0 @@
-// resource "azurerm_storage_account" "example" {
-//   name                     = "strikerst01"
-//   resource_group_name      = "striker-rg"
-//   location                 = "eastus2"
-//   account_tier             = "Standard"
-//   account_replication_type = "LRS"
-// }
