9b2d096b8937cacc72553c6d86b1762a0b30f338
maheshbabu7674 <112943607+maheshbabu7674@users.noreply.github.com>
2024-08-23 23:11:52 +0530

    Update provider.tf

--- resourcegroups/striker-rg/provider.tf
+++ resourcegroups/striker-rg/provider.tf
@@ -10,4 +10,8 @@ terraform {
 provider "azurerm"{
   skip_provider_registration = true
   features{}
+  subscription_id = "53ba0f30-95cd-4b0d-93e6-eec4280a84e4"
+  client_id       = "b8d16a1d-0ce0-448e-941c-ae2fe58ecb08"
+  client_secret   = "mqQ8Q~RkcxWoUsnPKRRYqd.tYaLR2AN9~bAtxctl"
+  tenant_id       = "731d7dd5-37f5-40fd-80b5-08b27409e481"
 }
